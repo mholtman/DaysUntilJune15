61f34a452983fdf31be97dc440b9aad444aeacfb
Eric Polerecky <eric@polerecky.com>
2015-02-06 09:34:05 -0500

    images are a pain in the butt.

--- DaysUntilJune15/DaysUntilJune15/MainContentPageViewModel.cs
+++ DaysUntilJune15/DaysUntilJune15/MainContentPageViewModel.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Reactive.Linq;
-using System.Windows.Input;
 using PropertyChanged;
-using Xamarin.Forms;
 
 namespace DaysUntilJune15
 {
@@ -14,29 +12,17 @@ namespace DaysUntilJune15
 
 		public string MainText { get; set; }
 
-		public string ButtonText { get; set; }
-
-		public ICommand RandomGifCommand { get; set; }
-
+	
 		public MainContentPageViewModel()
 		{
 			MainText = "How long until the mobile app building contest ends?";
-
-			ButtonText = "Show me a random gif?";
-
 			
-
 		//Reactive Programming
 		var timer = Observable.Interval(TimeSpan.FromSeconds(1));
 			timer.Subscribe(tick =>
 			{
 				var timeSpan = (new DateTime(2015, 06, 15) - DateTime.Now);
 				CountDown = string.Format("{0} days, {1} hours, {2} minutes, {3} seconds", timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
-			});
-
-			RandomGifCommand = new Command(() =>
-			{
-				Application.Current.MainPage.Navigation.PushModalAsync(new RandomGifPage());
 			});
 		}
 	}
