ebf7df1afc8ccca3a4b4a8803722fee0eefbf463
Eric Polerecky <eric@polerecky.com>
2015-02-05 22:05:39 -0500

    inital commit

---
+++ DaysUntilJune15/DaysUntilJune15.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using DaysUntilJune15.WinPhone.Resources;
+
+namespace DaysUntilJune15.WinPhone
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
---
+++ DaysUntilJune15/DaysUntilJune15/MainContentPage.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace DaysUntilJune15
+{
+	class MainContentPage : ContentPage
+	{
+		public MainContentPage()
+		{
+
+			var timeLable = new Label();
+
+
+			Content = new StackLayout
+			{
+				VerticalOptions = LayoutOptions.Center,
+				Children =
+				{
+					new Label
+					{
+						XAlign = TextAlignment.Center,
+						Text = "How long until the contest ends?"
+					},
+					timeLable
+				}
+			};
+
+		}
+
+	}
+}
